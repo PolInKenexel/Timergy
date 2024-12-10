c8573c91c7630bf5163fafd1304ee178eda24f32
samuel <samxd2020@gmail.com>
2024-12-10 14:07:48 -0600

    Esta es la verdadera primera version oficial, con nota de prueba

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
+<!-- una nota verdaderamente diferente -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
